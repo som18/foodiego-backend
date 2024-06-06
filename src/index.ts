2860fc85f29b41877ebf925a26389f836d4dda08
som18 <93382740+som18@users.noreply.github.com>
2024-06-06 20:33:22 +0530

    checkout session backend deploy

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from "./routes/MyUserRoutes";
 import{ v2 as cloudinary} from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
 .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -19,8 +20,11 @@ cloudinary.config({
 
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+
+app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({type: "*/*"}));
+app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
     res.send({ message: "health OK!" });
@@ -28,8 +32,9 @@ app.get("/health", async (req: Request, res: Response) => {
 
 
 app.use("/api/my/user", myUserRoute);
-app.use("/api/my/restaurant", myRestaurantRoute)
-app.use("/api/restaurant", restaurantRoute)
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order",orderRoute);
 
 app.listen(7000,()=>{
     console.log("Server started on localhost:7000");
